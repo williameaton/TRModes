5d404260fb5d48ce0769534f4c2508b518ab10ec
willeaton101 <weaton@princeton.edu>
2021-11-22 14:47:35 -0500

    "Created plot ABS outline"

--- plot_classes.py
+++ plot_classes.py
@@ -1,10 +1,11 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-class plot_spec():
+class plot_specs():
     # Class holds user-defined specifications for a plot they desire. An instance of this class is passed to
     # the plotter factory to generate the correct type of plot
 
-    # FUNCTIONS:
+    # Methods:
     def __init__(self, specs):
         self.plot_type
         self.in_fname
@@ -34,3 +35,53 @@ class plot_spec():
             raise ValueError(f"Plot type {plot_str} doesn't exist. Must be 'dispersion', '2D_radial' or '3D_animated")
 
 
+
+
+class gen_plot(ABC):
+    # Abstract base class. Sub-classes of this are classes to generate different types of plots
+    # E.g. a class called gen_dispersion
+
+    # Attributes
+    def __init__(self, plot_specs):
+        self.out_fname = plot_specs.out_fname
+        self.data # [p x 3] matrix where p is up to the user. Each row has n, l, omega loaded from .txt file
+        self.mpl_figure
+        self.mpl_axis
+        self.integration_required # Bool - true or false depending on if integration needed
+
+
+
+    # Methods:
+    def plot(self):
+        # Function is the driver for plotting - accessed by main codes. Will first prepare the plot. This incorporates
+        # any loading of the data and processing (e.g. integration). Then runs the make_plot function to actually make
+        # the plot
+        self.__prepare_plot()
+        self.__make_plot()
+        self.__output_plot()
+
+    @abstractmethod
+    def __make_plot(self):
+        # The actual plotting step - should return fig and axis to the plot function.
+        pass
+
+    @abstractmethod
+    def __load_data(self):
+        # Should include all loading/re-arrangement of data/any integration etc
+        pass
+
+    def __prepare_plot(self):
+        # Should include all loading/re-arrangement of data/any integration etc
+        self.__load_data()
+        if self.integration_required:
+            self.__integrate_mode()
+
+    def __integrate_mode(self, n, l, omega):
+        # I think this may be the same for each class type that requires the ability for integration. If not, then it
+        # will need to be an abstract method.
+        pass
+
+    @abstractmethod
+    def __output_plot(self):
+        # Some kind of generic function to save to a file
+        pass
