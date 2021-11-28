47c95af2a5480f907ff7a0c69539a0eed6c36c42
willeaton101 <weaton@princeton.edu>
2021-11-28 13:38:56 -0500

    Add initial 2D plot

--- 2D_radial_plot.py
+++ 2D_radial_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from plot_classes import gen_plot, plot_specs
+import matplotlib.animation as animation
 
 
 class radial_plot_2D(gen_plot):
@@ -9,6 +10,7 @@ class radial_plot_2D(gen_plot):
         self.out_fname = plot_specs.out_fname
         self.data_fname = plot_specs.in_fname
         self.data = None
+        self.z = None
         self.N= None
         self.L= None
         self.omega= None
@@ -28,7 +30,51 @@ class radial_plot_2D(gen_plot):
         self.output_plot()
 
     def make_plot(self):
-        # The actual plotting step - should return fig and axis to the plot function.
+        W = self.data*0.5
+        self.z = np.linspace(0, 1, len(W))
+
+        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+        dispersion, = ax.plot(W, self.z, linewidth=2)
+
+        # Plot zero axis:
+        ax.plot([0, 0], [0, np.amax(self.z)], 'k-', linewidth=1, alpha=0.7)
+
+        def init():  # only required for blitting to give a clean slate.
+            i = 0
+            dispersion.set_xdata(W * np.cos(i * 2 * np.pi / 100))
+            return dispersion,
+
+        def animate(i):
+            dispersion.set_xdata(W * np.cos(i * 2 * np.pi / 100))  # update the data.
+            return dispersion,
+
+        ani = animation.FuncAnimation(
+            fig, animate, init_func=init, interval=20, blit=False, save_count=50)
+
+        # Format the figure:
+        ax.set_theta_zero_location("N")
+        W_max = np.amax(np.abs(W))
+        ax.set_xlim([-W_max, W_max])
+        ax.set_ylabel("Relative Radius")
+        ax.set_xticklabels([])
+        ax.set_ylim(0, np.amax(self.z))
+        ax.grid(False)
+
+        # Detect and add zero-crossings:
+        zero_crossings = np.where(np.diff(np.sign(W)))[
+            0]  ## BY THE WAY THIS SHOULD BE EQUAL TO N - write a test - how will this work for crossing at 0 or R?
+
+        # Get z of zerocrossing:
+        rad_0x = self.z[zero_crossings]
+
+        theta = np.linspace(-W_max, W_max, 200)  # 200 is arbitrary
+        for i in range(len(rad_0x)):
+            line_rad = theta * 0 + rad_0x[i]
+            ax.plot(theta, line_rad, ":", color='r', linewidth=1)
+
+        ax.set_title(r"Toroidal mode ${}_{8}T_{50}$")
+        plt.show()
+
         pass
 
     def load_data(self, plot_specs):
