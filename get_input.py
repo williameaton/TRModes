517fc8fda8f3a2f2432479fdf3147f642e008366
pndabney <pdabney@princeton.edu>
2021-11-30 21:08:12 -0500

    Added to main driver code and reformatted comments

--- get_input.py
+++ get_input.py
@@ -1,23 +1,29 @@
-# Main code to process the user inputs and convert to a class that can be passed to the computations
-#
-# Last modified by pdabney@princeton.edu, 11/18/21
+# Last modified by pdabney@princeton.edu, 11/22/21
 
-#---------------------------------------------------------------------------------------------------                                    
-# Imports
+
 from Model.py import *
+from process_flags import *
+import sys, getopt, argparse
 
 
-#---------------------------------------------------------------------------------------------------
-def get_input():
 
-    # Process the command line inputs
-    process_flags()
+def get_input(argv):
+    # =====================================================================================
+    # DESCRIPTION:
+    #    Processes the user input and produces classes that will be used for computations
+    #    and/or animations and plots
+    #
+    #
+    # INPUT:
+    #    inputs         - Class containing information about the command line arguments
+    # =====================================================================================
+    
+    # Run through command line arguments and store inputs in the class Inputs                                
+    Inputs = process_input_args(sys.argv)
 
-    # Set the classes
-    set_class()
-
-
+    # Process flags and store in proper classes
+    model_class, output_class, control_class = process_flags(Inputs)
 
     return model_class, ouput_class, control_class
 
-#---------------------------------------------------------------------------------------------------                                    
+
