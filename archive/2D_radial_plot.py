a909feabd7cb20d3fd55e347a9ff02e7945e3a12
willeaton101 <weaton@princeton.edu>
2021-12-02 11:16:52 -0500

    switch plotting API

--- archive/2D_radial_plot.py
+++ archive/2D_radial_plot.py
@@ -1,38 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from plot_classes import gen_plot, plot_specs
+from plot_classes import gen_plot
+from plot_specs import plot_specs
 import matplotlib.animation as animation
 
 
 class radial_plot_2D(gen_plot):
     # Attributes
-    def __init__(self, plot_specs):
-        self.out_fname = plot_specs.out_fname
-        self.data_fname = plot_specs.in_fname
-        self.data = None
-        self.z = None
-        self.N = None
-        self.L = None
-        self.radius = plot_specs.radius
-        self.omega = None
-        self.mpl_figure = plot_specs.figure
-        self.mpl_axis = self._make_polar_axis(plot_specs)
-        self.animation=None
+    def __init__(self, ps):
+        self.out_fname            = ps.out_fname
+        self.data_fname           = ps.in_fname
+        self.data                 = None
+        self.z                    = None
+        self.N                    = None
+        self.L                    = None
+        self.radius               = ps.metadata.radius
+        self.omega                = None
+        self.mpl_figure           = ps.figure
+        self.mpl_axis             = self._make_polar_axis(ps)
+        self.animation            = None
         self.integration_required = False  # Bool - true or false depending on if integration needed
 
 
 
-    # Methods:
-    def plot(self, plot_specs):
-        # Function is the driver for plotting - accessed by main codes. Will first prepare the plot. This incorporates
-        # any loading of the data and processing (e.g. integration). Then runs the make_plot function to actually make
-        # the plot
-        super()._prepare_plot(plot_specs)
-        self._make_plot()
-        self._output_plot()
-
-
-
+    # METHODS:
     def _make_plot(self):
         W = self.data*0.5
         W_max = np.amax(np.abs(W))
@@ -71,9 +62,9 @@ class radial_plot_2D(gen_plot):
 
         self.mpl_axis.set_title(rf"Toroidal mode $ _{str(int(self.N))}T_{ str(int(self.L))}$")
 
+    # ------------------------------------------------------------------------------------------------------------------
 
-
-    def _load_radial_data(self, plot_specs):
+    def _load_radial_data(self, ps):
         # Expected file format is single column of data - first 3 lines are N, L, Omega. Next lines are displacement/
         # sensitivity at (increasing/decreasing) depth
         file_data = np.loadtxt(self.data_fname)
@@ -82,11 +73,14 @@ class radial_plot_2D(gen_plot):
         self.omega = file_data[2]
         self.data = file_data[3:]
 
+    # ------------------------------------------------------------------------------------------------------------------
 
     def _integrate_mode(self, n, l, omega):
         # I think this may be the same for each class type that requires the ability for integration. If not, then it
         # will need to be an abstract method.
         pass
+
+    # ------------------------------------------------------------------------------------------------------------------
 
     def _output_plot(self):
         # Write output to mp4 and gif
@@ -98,12 +92,20 @@ class radial_plot_2D(gen_plot):
             print(f"Writing to {out_str}...")
             self.animation.save(out_str, writer=writer)
 
-
+    # ------------------------------------------------------------------------------------------------------------------
 
     def _make_polar_axis(self, specs):
         return self.mpl_figure.add_subplot(specs.subaxis_pos, projection='polar')
 
+    # ------------------------------------------------------------------------------------------------------------------
+
     def _format_figure_metadata(self, W_max):
+        # ==============================================================================================================
+        # DESCRIPTION:
+        # INPUTS:
+        # OUTPUTS:
+        # ==============================================================================================================
+
         # Format the figure metadata e.g. titles etc:
         self.mpl_axis.set_theta_zero_location("N")
         self.mpl_axis.set_xlim([-W_max, W_max])
@@ -112,7 +114,7 @@ class radial_plot_2D(gen_plot):
         self.mpl_axis.set_ylim(0, np.amax(self.z))
         self.mpl_axis.grid(False)
 
-
+    # ------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -125,13 +127,14 @@ class radial_plot_2D(gen_plot):
 if __name__ == "__main__":
     # create dummy plot_specs object
     fig = plt.figure()
-    specs = plot_specs(type    = "2D_radial",
-                       inname  = "example_2D_data.txt",
-                       outname = "test",
-                       radius  = 6371,
-                       fig     = fig,
-                       subaxis_pos = 111)
-
+    specs = plot_specs(type        = "2D_radial",
+                       inname      = "example_2D_data.txt",
+                       outname     = "test",
+                       radius      = 6371,
+                       fig         = fig,
+                       subaxis_pos = 111,
+                       N           = 50,
+                       L           = 5)
 
     # Create gen_plot object
     r2D = radial_plot_2D(specs)
