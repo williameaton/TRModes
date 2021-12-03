d931ffa239536aef292f7d4040ea4920c334463b
Terance Schuh <tschuh@princeton.edu>
2021-12-03 11:43:40 -0500

    main driver code v1

---
+++ src/driver.py
@@ -0,0 +1,11 @@
+# driver.py
+#
+# this will be the main driver code that ties
+# input, calculation, and output together
+#
+# Originally written by tschuh-at-princeton.edu, 12/03/2021
+
+from toroidal_modes import toroidal_modes
+
+calculate = toroidal_modes()
+calculate.Tmodes_calculation()
