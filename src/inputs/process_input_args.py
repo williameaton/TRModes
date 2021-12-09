e332d26cc795f582c4c031db25480f2c9200c86f
pndabney <pdabney@princeton.edu>
2021-12-09 11:40:57 -0500

    Separate file for processing input arguments

---
+++ src/inputs/process_input_args.py
@@ -0,0 +1,64 @@
+# Last modified by pdabney@princeton.edu, 12/9/21
+
+#--------------------------------------------------------------------------------------------
+# Imports
+#--------------------------------------------------------------------------------------------
+
+import sys, getopt, argparse
+
+#--------------------------------------------------------------------------------------------
+# Define Empty Class
+#--------------------------------------------------------------------------------------------
+
+class user_inputs:
+    # Empty class to store input values
+    pass
+
+
+#--------------------------------------------------------------------------------------------
+# MAIN FUNCTION
+#--------------------------------------------------------------------------------------------
+
+def process_input_args(cml_arguments):
+    # =======================================================================================          
+    # DESCRIPTION:                                                                                   
+    #   Process the command line arguments and store values in a class
+    #                                                                                                 
+    # INPUT:                                                                                         
+    #    cml_arguments         - Command line arguments (i.e. sys.argv)
+    # OUTPUt:
+    #    inputs                - Class containing user inputs from the command line
+    # =======================================================================================
+    
+    # Create object that will hold information needed to parse command line
+    parser = argparse.ArgumentParser()
+    
+    # Include all command line flags
+    parser.add_argument("-gui", "--gui_launch", action="store_true", dest="gui", default=False, help="Launch GUI")
+
+    parser.add_argument("-eqr", "--equation_rho", dest="eq_rho", nargs=1, help="Equation for density")
+    parser.add_argument("-eqs", "--equation_vs", dest="eq_vs", nargs=1, help="Equation for shear velocity")
+    parser.add_argument("-eqp", "--equation_vp", dest="eq_vp", nargs=1, help="Equation for compressional velocity")
+    parser.add_argument("-mf", "--model_file", dest="model_file", nargs=1, help="Model file")
+    parser.add_argument("-rmin", "--radius_minimum", dest="r_min", nargs=1, help="Minimum radius")
+    parser.add_argument("-rmax", "--radius_maximum", dest="r_max", nargs=1, help="Maximum radius")
+    parser.add_argument("-Nr", "--number_rsteps", dest="Nr", nargs=1, help="Number of radial steps")
+    parser.add_argument("-n", "--n_values", dest="n", nargs=1, help="Radial order value(s)")
+    parser.add_argument("-nrange", "--nrange", dest="nrange", nargs=1, help="Range of radial order values")
+    parser.add_argument("-l", "--l_values", dest="l", nargs=1, help="Angular degree value(s)")
+    parser.add_argument("-lrange", "--lrange", dest="lrange", nargs=1, help="Range of angular degree values")
+    parser.add_argument("-int", "--integrator", dest="int_method", nargs=1, help="Integration method")
+    parser.add_argument("-mtype", "--mode_type", dest="mode_type", nargs=1, help="Type of mode: toroidal or radial")
+
+
+    # Flags related to output
+    parser.add_argument("-fig", "--figure", dest="figure_output", nargs="*", help="Figure output details")
+    parser.add_argument("-ofile", "--output_file", dest="output_file", nargs=1, help="File containing the computation outputs")
+                        
+    # Set class called inputs to store user input information
+    inputs = user_inputs()
+    
+    # Pass in command line arguments and store values in input class
+    parser.parse_args(str(cml_arguments), namespace=inputs)
+
+    return inputs
---
+++ src/inputs/process_input_fig.py
@@ -0,0 +1,125 @@
+# Last modified pdabney@princeton.edu, 12/9/21
+
+from process_flags import str2array, add2log
+
+
+def process_input_fig(str_in):
+    # =======================================================================================           
+    # DESCRIPTION:                                                                                     
+    #   Processes the users input regarding the output figure(s).
+    #                                                                                                 
+    # INPUT:                                                                                         
+    #    str_in         - String of the figure output information. (e.g. "figure1: 121 2D_radial
+    #                     L4 N2; 122 dispersion L1,2,3 N[1][1,2][1,2,3]")
+    # OUTPUT:
+    #    fname_out      - File name of figure(s). Variable is set to None if no name is specified.
+    #    ax_list        - List of figure axes. (e.g. ['121','122'])
+    #    L              - Angular order value(s). (format: 'L4' or 'L1,2,3,4' or L1-5)
+    #    N              - Radial order value(s). (format: 'N3' or 'N[1][3,4][1,2,3,4]' or  'N[1-6]'
+    #                    N_all[5]')
+    #    ptype          - Plot type: 'radial','toroidal'
+    # =======================================================================================  
+ 
+    if (str_in.find(":") != 1):
+        # Separate the output file name and store
+        fname_sep = str_in.split(":")
+        fname_out = fname_sep[0]
+        start = 1
+    else:
+        fname_sep = str_in
+        fname_out = None
+        start = 0
+
+    # Run through the remaining input
+    ax_list=[]
+    ptype=[]
+    L=[]
+    N=[]
+    for i in range(start, len(fname_sep)):
+        fig_sep = fname_sep[i].split(";")
+        for k in range(0,len(fig_sep)):
+            ax_sep = fig_sep[k].split(" ")
+            # Removes empty strings
+            new_axsep = [j for j in ax_sep if j]
+
+            # Store values
+            ax_list.append(new_axsep[0])
+            ptype.append(new_axsep[1].lower())
+
+            # Remove N and L identifier
+            l = new_axsep[2].replace('L','')
+            if len(l) > 1:
+                # If there is more than one value                                                      
+                if '-' in l:
+                    l_array = str2array(l,'-')
+                    l_values = range(l_array[0], l_array[1]+1)
+                else:
+                    l_values = str2array(l,',')
+            else:
+                l_values = [float(l)]  
+
+            # Apply the same n values for each l
+            if "n_all" in new_axsep[3].lower():
+                n = new_axsep[3].lower().replace('n_all','')
+                n_temp = n.replace('[','').replace(']','')
+                if len(n) > 1:
+                    # If there is more than a single value
+                    if '-' in n:
+                        n_array = sorted(str2array(n_temp,'-'))
+                        n_val = range(n_array[0], n_array[1]+1)
+                    else:
+                        n_val = str2array(n_temp,',')
+                else:
+                    n_val = float(n)
+
+                # Create sublists with the same n values for each l
+                n_values = [[]] * len(l_values)
+                for i in range(0,len(l_values)):
+                    n_values[i] =[* n_val]
+
+            # Apply different n values for each l
+            else:
+                n = new_axsep[3].replace('N','')
+                if len(n) > 1:
+                    # If there is more than a single value
+                    n_rep = n.replace('[','').replace(']',' ').split(' ')
+                    # Removes empty strings
+                    new_n = [j for j in n_rep if j]
+                    n_values = []
+                    for i in range(len(new_n)):
+                        # Appends an empty sublist inside the list that can be filled later
+                        n_values.append([])
+                        n_list = str2array(new_n[i],',')
+                        for k in range(len(n_list)):
+                            # Fill list with sublists
+                            n_values[i].append(n_list[k])
+                else:
+                    n_values = [float(n)]
+
+            # Store n and l values
+            L.append(l_values)
+            N.append(n_values)
+
+
+    # Check if all inputs are valid
+    assert len(L) == len(N), \
+        'Error: Must have corresponding number of n and l values. See Readme for details. \n'
+    # Check all N and L values are zero or greater and integers                                             
+    assert all(isinstance(x, int) for x in N), 'N values must be integers. \n'
+    assert N >= 0, 'N values must be zero or greater. \n'
+    assert all(isinstance(x, int) for x in L), 'L values must be integers. \n'
+    assert L >= 0, 'L values must be zero or greater. \n'
+    # Check is plot type exists                                                                             
+    assert ptype == 'dispersion' or ptype == 'radial_2d_plot', \
+        'Plot type does not exist. See Readme for details. \n'
+
+
+    # Add figure values to log
+    add2log("Figure Name", fname_out)
+    for j in range(0,ax_list):
+        add2log("    Axis", ax_list[j])
+        add2log("    Plot Type", ptype[j])
+        add2log("    L", L[j])
+        add2log("    N", N[j])
+    
+    return fname_out, ax_list, L, N, ptype
