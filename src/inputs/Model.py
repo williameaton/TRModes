f4b60199d2a33f9b2a2730f5ca9c859db74798f6
pndabney <pdabney@princeton.edu>
2021-12-09 23:27:39 -0500

    Added radial vector

---
+++ src/inputs/Model.py
@@ -0,0 +1,27 @@
+# Last modified by pdabney@princeton.edu, 11/30/21
+
+# Imports
+import numpy as np
+
+
+class Model:
+    # Attributes
+    def __init__(self,inputs):
+
+        # Model parameters
+        self.rho = inputs.rho
+        self.mu = inputs.mu
+        self.kappa = inputs.kappa
+        
+        self.r_min = inputs.r_min
+        self.r_max = inputs.r_max
+        self.dr = inputs.dr
+        self.rr = inputs.rr
+        
+        # Mode parameters
+        self.n = inputs.n
+        self.l = inputs.l
+        self.mtype = inputs.mtype
+
+        # Integration Method
+        self.method = inputs.int_method
