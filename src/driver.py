75da721cb2a799339919e464d200c5c754ac5d9d
pndabney <pdabney@princeton.edu>
2021-12-13 18:13:38 -0500

    Script driver code. It looks bad, but it runs

--- src/driver.py
+++ src/driver.py
@@ -13,8 +13,9 @@
 # Originally written by tschuh-at-princeton.edu, 12/03/2021
 # Last modified by tschuh-at-princeton.edu, 12/13/2021
 
+
 import numpy as np
-import sys
+import sys, argparse
 import os
 from calculations.toroidal_modes import toroidal_modes
 from inputs.Model import Model
@@ -55,26 +56,54 @@ from inputs.process_input_args import process_input_args
 # calculate = toroidal_modes(inputs)
 # calculate.Tmodes_calculation()
 
-
-def driver(sys):
+# inputs = process_input_args()                                                                            
+#def driver():
     
     # Store command line arguments in a class called inputs
-    inputs = process_input_args(sys.argv)
+    # inputs = process_input_args()
+parser = argparse.ArgumentParser()
 
+# Include all command line flags
+parser.add_argument("-gui", "--gui_launch", action="store_true", dest="gui", default=False, help="Launch GUI")
+    
+parser.add_argument("-eqr", "--equation_rho", dest="eq_rho", nargs=1, help="Equation for density")
+parser.add_argument("-eqs", "--equation_vs", dest="eq_vs", nargs=1, help="Equation for shear velocity")
+parser.add_argument("-eqp", "--equation_vp", dest="eq_vp", nargs=1, help="Equation for compressional velocity")
+parser.add_argument("-mf", "--model_file", dest="model_file", nargs=1, help="Model file")
+parser.add_argument("-rmin", "--radius_minimum", dest="r_min", nargs=1, help="Minimum radius")
+parser.add_argument("-rmax", "--radius_maximum", dest="r_max", nargs=1, help="Maximum radius")
+parser.add_argument("-Nr", "--number_rsteps", dest="Nr", nargs=1, help="Number of radial steps")
+parser.add_argument("-n", "--n_values", dest="n", nargs=1, help="Radial order value(s)")
+parser.add_argument("-nrange", "--nrange", dest="nrange", nargs=1, help="Range of radial order values")
+parser.add_argument("-l", "--l_values", dest="l", nargs=1, help="Angular degree value(s)")
+parser.add_argument("-lrange", "--lrange", dest="lrange", nargs=1, help="Range of angular degree values")
+parser.add_argument("-int", "--integrator", dest="int_method", nargs=1, help="Integration method")
+parser.add_argument("-mtype", "--mode_type", dest="mode_type", nargs=1, help="Type of mode: toroidal or radial")
+
+# Flags related to output
+parser.add_argument("-fig", "--figure", dest="figure_output", nargs="*", help="Figure output details")
+parser.add_argument("-ofile", "--output_file", dest="output_file", nargs=1, help="File containing the computation outputs")
+    
+    # Set class called inputs to store user input information
+#    inputs = user_inputs()
+
+# Pass in command line arguments and store values in input class
+inputs = parser.parse_args()
     #--------------------------------------------------------------------------------------------
     # CALCULATIONS:
     
     # If this attribute exists (which is require for calculations), modes will be computed
-    if hasattr(inputs, 'mode_type'):
-        # Process the user inputs 
-        model_class = process_inputs(inputs);
+if inputs.mode_type is not None:
+            
+    # Process the user inputs 
+    model_class = process_inputs(inputs);
 
-        # Compute modes
-        calculate = toroidal_modes(model_class)
-        calculate.Tmodes_calculation()
+    # Compute modes
+    calculate = toroidal_modes(model_class)
+    calculate.Tmodes_calculation()
 
-        # Returns output file name
-        inputs.output_filename = "lnw.txt"
+    # Returns output file name
+    inputs.output_filename = "lnw.txt"
 
 
     #--------------------------------------------------------------------------------------------
