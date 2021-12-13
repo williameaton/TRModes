02361eaab0b9761486a47ab231287fcc37a69cdc
Terance Schuh <tschuh@princeton.edu>
2021-12-13 12:47:00 -0500

    trying to get this to work

--- src/driver.py
+++ src/driver.py
@@ -17,64 +17,64 @@ import numpy as np
 import sys
 import os
 from calculations.toroidal_modes import toroidal_modes
-#from inputs.Model import Model
-#from inputs.process_inputs import process_inputs
-#from inputs.process_input_fig import process_input_fig
-#from inputs.process_input_args import process_input_args
+from inputs.Model import Model
+from inputs.process_inputs import process_inputs
+from inputs.process_input_fig import process_input_fig
+from inputs.process_input_args import process_input_args
 #from plotting.ps_figure import ps_figure
 #from plotting.ps_axis import ps_axis
 
-class dummy_inputs():
-    def __init__(self):
-        # physical planet values
-        self.r_max = 6371000  # Earth radius [m]
-        self.r_min = 2891000 # CMB radius [m]
-        self.nr = 100 # number of nodes in the radial dimension
-        self.dr = np.empty((self.nr))
-        self.dr.fill((self.r_max - self.r_min)/(self.nr - 1))
-        for i in range(1,self.nr):
-            self.rr = self.r_min + np.dot((np.arange(0,self.nr)),self.dr[i]) # vector for Earth's radii [m]
+# class dummy_inputs():
+#     def __init__(self):
+#         # physical planet values
+#         self.r_max = 6371000  # Earth radius [m]
+#         self.r_min = 2891000 # CMB radius [m]
+#         self.nr = 100 # number of nodes in the radial dimension
+#         self.dr = np.empty((self.nr))
+#         self.dr.fill((self.r_max - self.r_min)/(self.nr - 1))
+#         for i in range(1,self.nr):
+#             self.rr = self.r_min + np.dot((np.arange(0,self.nr)),self.dr[i]) # vector for Earth's radii [m]
 
-        # mode parameters
-        # cant compute n=0, l=1 mode
-        self.nrange = [0, 5] # n >= 0
-        self.lrange = [1, 3, 5] # l >= 1
+#         # mode parameters
+#         # cant compute n=0, l=1 mode
+#         self.nrange = [0, 5] # n >= 0
+#         self.lrange = [1, 3, 5] # l >= 1
 
-        # integration method
-        self.method = 'euler'
+#         # integration method
+#         self.method = 'euler'
 
-        # create earth's density and shear profile for homogeneous spherical Earth model
-        self.rho = np.empty((self.nr))
-        self.rho.fill(4380) # mean density [kg/m^3]
-        self.mu = np.empty((self.nr))
-        self.mu.fill(4380*(5930**2)) # shear modulus [Pa] = rho0*(vs0)^2
+#         # create earth's density and shear profile for homogeneous spherical Earth model
+#         self.rho = np.empty((self.nr))
+#         self.rho.fill(4380) # mean density [kg/m^3]
+#         self.mu = np.empty((self.nr))
+#         self.mu.fill(4380*(5930**2)) # shear modulus [Pa] = rho0*(vs0)^2
 
-inputs = dummy_inputs()
+# inputs = dummy_inputs()
 
-# do calculations
-calculate = toroidal_modes(inputs)
-calculate.Tmodes_calculation()
+# # do calculations
+# calculate = toroidal_modes(inputs)
+# calculate.Tmodes_calculation()
 
 
-# def driver(sys):
+def driver(sys):
     
-#     # Store command line arguments in a class called inputs
-#     inputs = process_input_args(sys.argv)
+    # Store command line arguments in a class called inputs
+    inputs = process_input_args(sys.argv)
 
-#     #--------------------------------------------------------------------------------------------
-#     # CALCULATIONS:
+    #--------------------------------------------------------------------------------------------
+    # CALCULATIONS:
     
-#     # If this attribute exists (which is require for calculations), modes will be computed
-#     if hasattr(inputs, 'mode_type'):
-#         # Process the user inputs 
-#         model_class = process_inputs(inputs);
+    # If this attribute exists (which is require for calculations), modes will be computed
+    if hasattr(inputs, 'mode_type'):
+        # Process the user inputs 
+        model_class = process_inputs(inputs);
 
-#         # Compute modes
-#         calculate = toroidal_modes(model_class)
-#         calculate.Tmodes_calculation()
+        # Compute modes
+        calculate = toroidal_modes(model_class)
+        calculate.Tmodes_calculation()
 
-#         # Returns output file name
-#         inputs.output_filename = "lnw.txt"
+        # Returns output file name
+        inputs.output_filename = "lnw.txt"
 
 
     #--------------------------------------------------------------------------------------------
