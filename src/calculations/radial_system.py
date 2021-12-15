cce66ec70dc20f88106cc5fd9bf46074a9c5a148
Terance Schuh <tschuh@princeton.edu>
2021-12-15 14:38:42 -0500

    coupled system of ODEs for radial oscillations

---
+++ src/calculations/radial_system.py
@@ -0,0 +1,17 @@
+# radial_system.py
+#
+# 6 coupled 1st-order ODEs for U, V, P, R, S, and B
+# that govern radial oscillations
+#
+# Originally written by tschuh-at-princeton.edu, 12/15/2021
+
+import numpy as np
+
+def radial_system(U,V,P,R,S,B,w,r,rho,mu,kappa,k2,l):
+    dU_dr =
+    dV_dr =
+    dP_dr =
+    dR_dr =
+    dS_dr =
+    dB_dr =
+    return dU_dr, dV_dr, dP_dr, dR_dr, dS_dr, dB_dr;
