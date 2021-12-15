638b491f113f6039839791b7e34c5a28d4ed789f
Terance Schuh <tschuh@princeton.edu>
2021-12-15 17:07:48 -0500

    more modular ab2 method

--- src/calculations/ab2.py
+++ src/calculations/ab2.py
@@ -1,11 +1,12 @@
 # ab2.py
 #
 # Originally written by tschuh-at-princeton.edu,12/05/2021
+# Last modified by tschuh-at-princeton.edu, 12/15/2021
 
 import numpy as np
 from calculations.toroidal_system import toroidal_system
 
-def ab2(w,dr,rr,rho,mu,l):
+def ab2(mtype,w,dr,rr,rho,mu,l):
 
     #############################################################
     
@@ -25,14 +26,15 @@ def ab2(w,dr,rr,rho,mu,l):
     counter = 0
     for i in range(1,nr):
 
-        if counter == 0:
-            # use Euler method for 1st step
-            W[i] = W[i-1] + dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[0]
-            T[i] = T[i-1] + dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[1]
-            counter = 1
-        else:
-            W[i] = W[i-1] + (3/2)*dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[0] - (1/2)*dr[i-2]*toroidal_system(W[i-2],T[i-2],w,rr[i-2],rho[i-2],mu[i-2],k2)[0]
-            T[i] = T[i-1] + (3/2)*dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[1] - (1/2)*dr[i-2]*toroidal_system(W[i-2],T[i-2],w,rr[i-2],rho[i-2],mu[i-2],k2)[1]
+        if mtype == 'toroidal':
+            if counter == 0:
+                # use Euler method for 1st step
+                W[i] = W[i-1] + dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[0]
+                T[i] = T[i-1] + dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[1]
+                counter = 1
+            else:
+                W[i] = W[i-1] + (3/2)*dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[0] - (1/2)*dr[i-2]*toroidal_system(W[i-2],T[i-2],w,rr[i-2],rho[i-2],mu[i-2],k2)[0]
+                T[i] = T[i-1] + (3/2)*dr[i-1]*toroidal_system(W[i-1],T[i-1],w,rr[i-1],rho[i-1],mu[i-1],k2)[1] - (1/2)*dr[i-2]*toroidal_system(W[i-2],T[i-2],w,rr[i-2],rho[i-2],mu[i-2],k2)[1]
             
     # count zero crossings
     # must integer divide (//) by 2 at the end because np.where
